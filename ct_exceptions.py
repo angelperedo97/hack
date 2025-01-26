ce30eabb818bb7c8a7b7e3f98b79145b41b74810
angelperedo97 <162521608+angelperedo97@users.noreply.github.com>
2025-01-26 09:26:34 -0600

    Update and rename ct_ecxeptions.py to ct_exceptions.py

--- ct_exceptions.py
+++ ct_exceptions.py
@@ -161,4 +161,4 @@ async def process_image_endpoint(file: UploadFile = File(...)):
 # -----------------------------
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("ct:app", host="0.0.0.0", port=port)
+    uvicorn.run("ct_exceptions:app", host="0.0.0.0", port=port)
